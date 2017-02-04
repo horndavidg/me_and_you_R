be6c63a10f69d39445cd06770f853eebab28a056
David Horn <horndavidg@gmail.com>
2017-02-03 17:57:51 -0800

    fixed mailer sender name and kudo mailer spacing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "help@meandyou.com"
+  default from: "Me & You <help@meandyou.com>"
   default_url_options[:host] = "localhost:3000"
 end
